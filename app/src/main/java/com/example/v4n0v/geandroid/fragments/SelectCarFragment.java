42b2d88a32a462df2eaf3d7be22b77bceedbc3dc
v4n0v <avetchinov@gmail.com>
2018-03-02 17:44:58 +0300

    2_02_18 стиль кнопки (нажата\не нажата) в bottom_sheet

---
+++ app/src/main/java/com/example/v4n0v/geandroid/fragments/SelectCarFragment.java
@@ -0,0 +1,32 @@
+package com.example.v4n0v.geandroid.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.v4n0v.geandroid.R;
+
+/**
+ * Created by v4n0v on 02.03.18.
+ */
+
+public class SelectCarFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.select_car_fragment, container, false);
+        initViews(view);
+
+        return view;
+    }
+
+
+    private void initViews(View view) {
+
+    }
+}
