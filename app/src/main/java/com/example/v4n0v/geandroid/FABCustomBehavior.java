82a61c6a5e9691d0510d19dc8730ed11966abc77
v4n0v <avetchinov@gmail.com>
2018-03-09 13:12:09 +0300

    9_03_18 CoordinatorLayout в отдельной активности, открывается поумолчанию
    4. *Разобраться с кодом “уплывания” fab в классе behavior
    5. *Переделать последний пример так, чтобы fab “уплывала” не вниз, а за правую границу
    экрана.

---
+++ app/src/main/java/com/example/v4n0v/geandroid/FABCustomBehavior.java
@@ -0,0 +1,53 @@
+package com.example.v4n0v.geandroid;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by v4n0v on 09.03.18.
+ */
+
+public class FABCustomBehavior extends FloatingActionButton.Behavior{
+
+    String TAG = "FAB_CUSTOM";
+
+    public FABCustomBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        Log.d(TAG, "onNested");
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+
+    }
+
+    @Override
+    public void onStopNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View target, int type) {
+
+        child.animate().translationX(0).scaleX(1).scaleY(1).setDuration(300).start();
+        super.onStopNestedScroll(coordinatorLayout, child, target, type);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                                       @NonNull FloatingActionButton child,
+                                       @NonNull View directTargetChild,
+                                       @NonNull View target,
+                                       int axes, int type) {
+
+        Log.d(TAG, "onStartNested");
+
+        child.animate().translationX(250).scaleX(0).scaleY(0).setDuration(300).start();
+
+
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+}
